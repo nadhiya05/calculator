8b7d9c50409119dcdbfd2abc9bddff4918df2e84
nadhiya05 <yahooakshaya123@gmail.com>
2021-02-25 15:21:45 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,11 @@
+function btclick(val){
+    document.getElementById("screen").value+=val;
+}
+function clearscrn(){
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
